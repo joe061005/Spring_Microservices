0fb63dfe8be38fcc533e2bb39a0e7d206e09ea3e
Joe <18233368@life.hkbu.edu.hk>
2023-08-16 20:58:08 +0800

    Secure Microservices using Keycloak

---
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+
+        // .pathMathcers(..).permitALL() -> disable security checks for the particular calls
+        // .anyExchange().authenticated() -> enable security checks for other calls
+        serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
+        return serverHttpSecurity.build();
+
+    }
+}
