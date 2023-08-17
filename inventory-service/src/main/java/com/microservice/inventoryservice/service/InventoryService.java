718dd9f74e07f0b0a342d123e716c3caa243a1f8
Joe <18233368@life.hkbu.edu.hk>
2023-08-17 21:12:19 +0800

    implement Circuit Breaker (retry/return error when inventory service is down)

--- inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.microservice.inventoryservice.service;
 import com.microservice.inventoryservice.dto.InventoryResponse;
 import com.microservice.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +12,19 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+// log statement
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    // try {...} throw Exception (...) {...}
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
+//        log.info("Wait Started");
+//        Thread.sleep(10000);
+//        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(
             inventory ->
                 InventoryResponse.builder()
